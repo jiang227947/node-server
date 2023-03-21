e2c738f8308c3cc722b049dcf96dc30a7d282cbb
jzywd <jzywd666@163.com>
2023-03-21 21:30:02 +0800

    [M] 1. 修改文件路径

--- server/dist/routes/product.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_1 = require("../controllers/product");
-const router = (0, express_1.Router)();
-router.get('/', product_1.getProducts);
-exports.default = router;
